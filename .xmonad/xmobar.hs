9c6087332dafe9ebd1367a3362e8bbd050459fb4
Narthorn <narthorn@gmail.com>
2015-09-26 10:22:13 +0200

    xmobar: Use both AutoMPD and MPD monitors at the same time
    I am literally a genius.

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -7,7 +7,8 @@ Config {
     position = Static { xpos = 1680, ypos = 0, width = 1800, height = 16 },
 	lowerOnStart = True,
     commands = [
-        Run MPD              ["-t","<fc=#FFFFCC><statei></fc> <state>: <fc=#CEFFAC><artist> - <title> (<length>)</fc> <fc=#FFFFCC><bar></fc> [<flags>]", "-W", "25", "-b", "-", "-f", "#"] 10, 
+        Run AutoMPD          ["-t","<fc=#FFFFCC><statei></fc> <state>: <fc=#CEFFAC><artist> - <title> (<length>)</fc>"],
+        Run MPD              ["-t","<fc=#FFFFCC><bar></fc> [<flags>]", "-W", "25", "-b", "-", "-f", "#"] 10, -- AutoMPD won't wake up to update progres bar, so use regular 1s polling
         Run MultiCpu         ["-t","Cpu: <total0> <total1> <total2> <total3> <total4> <total5>","-L","30",   "-H","60",   "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory           ["-t","Mem: <usedratio>%",                                         "-L","4096", "-H","8192", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap             ["-t","Swap: <usedratio>%",                                        "-L","512",  "-H","1024", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
@@ -22,5 +23,5 @@ Config {
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{%mpd%       Gpu: <fc=#CEFFAC>%gputemp%</fc>  %multicpu%   %memory%   %swap%   %enp3s0%   Keys: <fc=#CEFFAC>%kbd%</fc>   <fc=#FFFFCC>%date%</fc>"
+    template = "%StdinReader% }{%autompd% %mpd%       Gpu: <fc=#CEFFAC>%gputemp%</fc>  %multicpu%   %memory%   %swap%   %enp3s0%   Keys: <fc=#CEFFAC>%kbd%</fc>   <fc=#FFFFCC>%date%</fc>"
 }
