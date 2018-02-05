5c32db72fad3a29ccbd2bb171e937a2eeb2c2b5d
Narthorn <narthorn@gmail.com>
2018-02-05 04:27:41 +0100

    xmobar: change cpu to autovbars, reduce size of mpd bar and add disk free space

--- .config/xmonad/xmobar.hs
+++ .config/xmonad/xmobar.hs
@@ -1,7 +1,7 @@
 -- This is setup for dual {1680x1050, 1920x1080} monitors, with the right monitor as primary
 -- and 120px space for trayer dock
 Config {
-    font = "xft:Misc Fixed:pixelsize=11,IPAGothic:pixelsize=11",
+    font = "xft:Misc Fixed:pixelsize=11,IPAGothic:pixelsize=11,xos4 Terminus:pixelsize=11",
     bgColor = "#000000",
     fgColor = "#ffffff",
     position = Static { xpos = 1680, ypos = 0, width = 1800, height = 16 },
@@ -9,8 +9,9 @@ Config {
     lowerOnStart = True,
     commands = [
         Run AutoMPD          ["-t","<fc=#FFFFCC><statei></fc> <state>: <fc=#CEFFAC><artist> - <title> (<length>)</fc>"],
-        Run MPD              ["-t","<fc=#FFFFCC><bar></fc> [<flags>]", "-W", "25", "-b", "-", "-f", "#"] 10, -- AutoMPD won't wake up to update progres bar, so use regular 1s polling
-        Run MultiCpu         ["-t","<total0> <total1> <total2> <total3> <total4> <total5> <total6> <total7> <total8> <total9> <total10> <total11>","-L","30",   "-H","60",   "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run MPD              ["-t","<fc=#FFFFCC><bar></fc> [<flags>]", "-W", "10", "-b", "-", "-f", "#"] 10, -- AutoMPD won't wake up to update progres bar, so use regular 1s polling
+        Run MultiCpu         ["-t","<autovbar>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 5,
+        Run DiskU            [("/", "/ <free>")] ["-L","20","-H","80","-h","#CEFFAC","-l","#FFB6B0","-n","#FFFFCC","-S","true"] 10,
         Run Memory           ["-t","<usedratio>%",                                         "-L","4096", "-H","8192", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Swap             ["-t","<usedratio>%",                                        "-L","512",  "-H","1024", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Network "enp3s0" ["-t","Net: <rx>, <tx>",                                 "-m","4", "-L","10",   "-H","1000", "-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
@@ -25,5 +26,11 @@ Config {
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %autompd% %mpd%       Gpu: <fc=#CEFFAC>%gputemp%</fc>    Cpu: <fc=#CEFFAC>%cputemp%</fc> %multicpu%    Mem: %memory% (%swap%)    %enp3s0%    Keys: <fc=#CEFFAC>%kbd%</fc>    <fc=#FFFFCC>%date%</fc>"
+    template = "%StdinReader% }{ %autompd% %mpd%       Gpu: <fc=#CEFFAC>%gputemp%</fc>    Cpu: <fc=#CEFFAC>%cputemp%</fc> %multicpu%  %disku%    Mem: %memory% (%swap%)    %enp3s0%    Keys: <fc=#CEFFAC>%kbd%</fc>    <fc=#FFFFCC>%date%</fc>"
 }
+
+-- cpu meter ideas
+--
+-- symbols:  ‧ ⁖ ⁘ ⁙ ⁜
+-- more :    ⠏ ⠕ ⠛ ⠟   http://graphemica.com/blocks/braille-patterns
+-- play with font options (pixelsize, style)
